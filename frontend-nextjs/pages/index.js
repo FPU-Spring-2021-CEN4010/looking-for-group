f83f9fa364ee95e57a1aae9c46961825bb28d459
Braden Cariaga <11431488+BCBEST@users.noreply.github.com>
2021-03-27 15:49:38 -0400

    Added Navigation

--- frontend-nextjs/pages/index.js
+++ frontend-nextjs/pages/index.js
@@ -1,13 +1,15 @@
 import Header from '../components/Header'
-import HowToUse from '../components/navigation/HowToUse'
+import Navigation from '../components/Navigation'
 
 function HomePage() {
 
      
      return (
           <div>
-               <Header name="Yo mama" />
-               <HowToUse />
+               <div className="container">
+                    <Header name="Yo mama" />
+                    <Navigation />
+               </div>
           </div>
          
      )
