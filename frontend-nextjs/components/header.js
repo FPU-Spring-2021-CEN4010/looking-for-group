0b82d10c37dab314906c5e322ea25f22d6a02e37
Braden Cariaga <11431488+BCBEST@users.noreply.github.com>
2021-03-27 12:29:31 -0400

    Added Sample Header File

---
+++ frontend-nextjs/components/header.js
@@ -0,0 +1,9 @@
+function Header({name}) {
+
+
+     return (
+          <div><h1>I am your header... { name }</h1></div>
+     )
+}
+
+module.exports = Header
--- frontend-nextjs/pages/index.js
+++ frontend-nextjs/pages/index.js
@@ -1,7 +1,11 @@
+import Header from '../components/Header'
+
 function HomePage() {
-     return <div>Welcome to Next.js! 
-          My name Jeff!
-     </div>
+
+     
+     return (
+          <Header name="Hello World" />
+     )
 }
 
 export default HomePage
