955a23014d619c1373f772b1a8b8c4e571d9bcff
veryhalalguy <61290708+veryhalalguy@users.noreply.github.com>
2021-03-28 21:08:48 -0400

    Updated Styling
    - Added Margins
    - Reverted commit #012a555a29a1fcd2548c0ee666e03e39486051f1

--- frontend-nextjs/components/ViewAdvertisements.js
+++ frontend-nextjs/components/ViewAdvertisements.js
@@ -3,7 +3,7 @@ import Advertisement from "./Advertisement"
 function ViewAdvertisements() {
 
          return (
-              <div className="color-text-white">
+              <div>
                 <div className="advertisements">
                     <Advertisement />                    
                     <Advertisement />   
