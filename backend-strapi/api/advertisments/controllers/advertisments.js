ba1357bf5b654537685ea3f7aeb6a8e8f86cb06d
Braden Cariaga <11431488+BCBEST@users.noreply.github.com>
2021-03-30 18:54:44 -0400

    New Fields API Route
    For fetching all active fields

--- backend-strapi/api/advertisments/controllers/advertisments.js
+++ backend-strapi/api/advertisments/controllers/advertisments.js
@@ -20,4 +20,24 @@ module.exports = {
           const entity = await strapi.services.advertisments.delete({ id });
           return sanitizeEntity(entity, { model: strapi.models.advertisments });
      },
+
+     async fields(ctx) {
+          const Comms = await strapi.services.comms.find();
+          const Game_Modes = await strapi.services["game-modes"].find();
+          const Games = await strapi.services.games.find();
+          const Platforms = await strapi.services.platforms.find();
+          const Player_Roles = await strapi.services["player-role"].find();
+          const Regions = await strapi.services.regions.find();
+
+          const Options = {
+               Comms: await strapi.services.fields.serializeField(Comms),
+               Game_Modes: await strapi.services.fields.serializeField(Game_Modes),
+               Games: await strapi.services.fields.serializeField(Games),
+               Platforms: await strapi.services.fields.serializeField(Platforms),
+               Player_Roles: await strapi.services.fields.serializeField(Player_Roles),
+               Regions: await strapi.services.fields.serializeField(Regions)
+          }
+
+          return Options;
+     }
 };
