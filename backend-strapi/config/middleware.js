84e6b1875f2a8eac440ca90bc6378a59dd475cf5
Braden Cariaga <11431488+BCBEST@users.noreply.github.com>
2021-04-08 12:03:24 -0400

    Added accepted addresses

--- backend-strapi/config/middleware.js
+++ backend-strapi/config/middleware.js
@@ -2,7 +2,7 @@ module.exports = {
      settings: {
           cors: {
                enabled: true,
-               origin: ["http://localhost:3000", "http://localhost:1337", "http://0.0.0.0:1337", "http://0.0.0.0:3000", "https://abd.bcariaga.me", "https://abdapi.bcariaga.me"]
+               origin: ["http://localhost:3000", "http://localhost:1337", "http://0.0.0.0:1337", "http://0.0.0.0:3000", "https://abd.bcariaga.me", "https://abdapi.bcariaga.me", "http://192.168.1.240:3000", "http://192.168.1.240:1337"]
           }
      }
 }
