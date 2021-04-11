fafcc3eaa23299738d7666374dbba449023d6d38
Syzco <bradencariaga@hotmail.com>
2021-04-10 20:11:55 -0400

    added console logs

--- cleanup/app.js
+++ cleanup/app.js
@@ -6,9 +6,13 @@ require('dotenv').config();
  * Schedule the cleaner to run once, daily. 
  */
 const job = Schedule.scheduleJob({hour: process.env.RUNTIME_HR, minute: process.env.RUNTIME_MIN}, async function() {
+     console.log("Starting daily cleaning process.");
+
      //Instantiate the cleaner process for this instance. 
      let dailyCleaner = new Cleaner(process.env.CL_USER, process.env.CL_PASS, process.env.API_URL);
 
      //Start cleaning process.
      await dailyCleaner.cleanProcess();
+
+     console.log("Finished daily cleaning process.!");
 })
