3565059b66241b6af891179599f97ea4c067f9a3
Braden Cariaga <bradencariaga@hotmail.com>
2021-04-15 16:32:27 -0400

    Added validation on modify for missing fields

--- frontend-nextjs/components/popups/ModifyGroup.js
+++ frontend-nextjs/components/popups/ModifyGroup.js
@@ -12,6 +12,7 @@ import {mutate} from 'swr'
 function ModifyGroup({id, initialValues, close, fields, filterFunc}) {
      const [formData, updateFormData] = useState(initialValues);
 
+<<<<<<< Updated upstream
 
      /**
       * @method validateGroup
@@ -19,7 +20,29 @@ function ModifyGroup({id, initialValues, close, fields, filterFunc}) {
       * @returns 
       */
      const verifyValues = () => {
+=======
+     const [validation, updateValidation] = useState({
+          state: false,
+          errMessage: ""
+     })
 
+     const validate = async () => {
+          return new Promise((resolve, reject) => {
+               let formKeys = Object.keys(formData);
+               formKeys.forEach((v, i) => {
+                    let data = formData[v];
+                    if (data == "" || data == null || data.length <= 0) {
+                         console.log(data);
+                         reject("EMPTY_VAL");
+                    }
+
+                    if (i == formKeys.length-1) resolve();
+               });
+          })
+     }
+>>>>>>> Stashed changes
+
+     const verifyValues = () => {
           // pull the updated information from the current advertisement
           let updateData = {...formData};
 
@@ -55,38 +78,84 @@ function ModifyGroup({id, initialValues, close, fields, filterFunc}) {
           return updateData;
      }
 
+<<<<<<< Updated upstream
      /**
       * @method updateGroup
       * @description Updates the group
       * @param {*} e 
       */
      const updateGroup = (e) => {
+=======
+     const updateGroup = async (e) => {
+>>>>>>> Stashed changes
           e.preventDefault();
 
-          // pass through verify values to make sure values are ints
-          let updateData = verifyValues();
+          //Validate the form values.
+          try {
+               await validate();
+               updateValidation({
+                    state: true,
+                    errMessage: ""
+               });
+          } catch(err) {
+               if (err == "EMPTY_VAL") {
+                    updateValidation({
+                         state: false,
+                         errMessage: "Not all form values are entered. Please make sure all fields are filled."
+                    });
+               }
+               console.log(err);
+          }
 
-          // attempt a request to update the advertisement
-          axios.put("/advertisments/"+id, updateData).then((res) => {
-               if (res.status == 200) { // successful request 
-                    filterFunc("");
-                    mutate("/advertisments");
-                    close();
-               }
-          }).catch((err) => { // lmao we got an error
-               if (err.message == "405 Method Not Allowed") {
-                    close();
-               }
-          });
+          //Check if validation passed.
+          if (validation.state == true && validation.errMessage == "") {
+               // pass through verify values to make sure values are ints
+               let updateData = verifyValues();
+
+               // attempt a request to update the advertisement
+               axios.put("/advertisments/"+id, updateData).then((res) => {
+                    if (res.status == 200) { // successful request 
+                         filterFunc("");
+                         mutate("/advertisments");
+                         close();
+                    }
+               }).catch((err) => { // lmao we got an error
+                    if (err.message == "405 Method Not Allowed") {
+                         close();
+                    }
+               });
+          }
      }
 
-     return ( 
-          <div>
-               <div className="popup modGroup">                    
-                    <CreationForm initialValues={formData} funcState={updateFormData} funcSubmit={updateGroup} title="" hideCancel={false} funcClose={close} fields={fields} />
+     //Close the invalid popup. 
+     const closeInvalid = () => {
+          updateValidation({
+               state: false,
+               errMessage: ""
+          })
+     }
+
+     if(validation.errMessage != "") { // html for the invalid popup where we instruct the user that they are NOT the owner of this group so they cannot delete it
+          return (
+               <div>
+                    <div className="popup">
+                         <h3>Error Modifying!</h3>
+      
+                         <p>{validation.errMessage}</p>
+      
+                         <button id="invalidOwnerPopup" type="button" onClick={closeInvalid}>Close</button>
+                    </div>
                </div>
-          </div>
-     )
+          )
+     } else {
+          return ( 
+               <div>
+                    <div className="popup modGroup">                    
+                         <CreationForm initialValues={formData} funcState={updateFormData} funcSubmit={updateGroup} title="" hideCancel={false} funcClose={close} fields={fields} />
+                    </div>
+               </div>
+          )
+     }
 }
 
 module.exports = ModifyGroup
